5f50872aeda972197c7bf8ead5f17d710eab500f
Israel Belchior <israel.pereira.belchior@gmail.com>
2025-06-21 22:19:44 +0000

    fix: putting some new functions

--- number_guess.sh
+++ number_guess.sh
@@ -5,13 +5,20 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --noalign -c"
 echo "Enter your username:"
 read NAME
 
-if [[ -z $NAME ]]
-then
-  
-done
-FIND_NAME=$($PSQL "SELECT username FROM users WHERE username = '$NAME'";)
-
-
 GET_RANDOM(){
+  
+FIND_NAME=$($PSQL "SELECT username FROM users WHERE username = '$NAME';")
+GAMES=$($PSQL "SELECT games_played FROM users WHERE username = '$NAME';")
+BEST=$($PSQL "SELECT best_game FROM users WHERE username = '$NAME';")
+
+if [[ -z $FIND_NAME ]]
+then
+  INSERT_NAME=$($PSQL "INSERT INTO users(username) VALUES('$NAME');")
+  echo -e "\n Welcome, $NAME! It looks like this is your first time here."
+else
+  echo -e "\nWelcome back, $NAME! You have played $GAMES games, and your best game took $BEST guesses."
+fi
+
+
   echo $((1 + RANDOM % 1000))
 }

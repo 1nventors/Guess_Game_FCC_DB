04c9679b09233d0b9ec44e17b58fd43aa0df83ee
Israel Belchior <israel.pereira.belchior@gmail.com>
2025-06-21 22:09:56 +0000

    fix: trying some functions

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,16 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --noalign -c"
 
+echo "Enter your username:"
+read NAME
+
+if [[ -z $NAME ]]
+then
+  
+done
+FIND_NAME=$($PSQL "SELECT username FROM users WHERE username = '$NAME'";)
+
+
 GET_RANDOM(){
   echo $((1 + RANDOM % 1000))
 }
-GET_RANDOM

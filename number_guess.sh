73d9debc3e86e3c6e4267accbe5c6bb9d0896d7d
Israel Belchior <israel.pereira.belchior@gmail.com>
2025-06-21 22:56:30 +0000

    feat: final update

--- number_guess.sh
+++ number_guess.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --noalign -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read NAME
 
 GET_RANDOM(){
 
-TRIES = 0
+TRIES=0
 
 SECRET=$((1 + RANDOM % 1000))
 FIND_NAME=$($PSQL "SELECT username FROM users WHERE username = '$NAME';")
@@ -17,7 +17,7 @@ BEST=$($PSQL "SELECT best_game FROM users WHERE username = '$NAME';")
 if [[ -z $FIND_NAME ]]
 then
   INSERT_NAME=$($PSQL "INSERT INTO users(username) VALUES('$NAME');")
-  echo -e "\n Welcome, $NAME! It looks like this is your first time here."
+  echo -e "\nWelcome, $NAME! It looks like this is your first time here."
 else
   echo -e "\nWelcome back, $NAME! You have played $GAMES games, and your best game took $BEST guesses."
 fi
@@ -43,10 +43,13 @@ elif [[ $GUESS -gt $SECRET ]]
 then
   echo -e "\nIt's lower than that, guess again:"
 else
-  echo "You guessed it in $TRIES tries. The secret number was $RANDOM. Nice Job!"
+  echo "You guessed it in $TRIES tries. The secret number was $SECRET. Nice Job!"
   break
 fi
 done
+
+
+UPDATE_STATS=$($PSQL "UPDATE users SET games_played = games_played + 1, best_game = CASE WHEN best_game IS NULL OR $TRIES < best_game THEN $TRIES ELSE best_game END WHERE username = '$NAME';")
 
 }
 
